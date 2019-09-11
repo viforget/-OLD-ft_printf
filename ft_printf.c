9274d6cbf8c259fc9b7163e884d9da81d93da80c
Victor Forget <viforget@student.s19.be>
2019-09-11 09:45:01 +0200

    advance in mains functions

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: viforget <viforget@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/10 12:58:30 by viforget          #+#    #+#             */
-/*   Updated: 2019/09/10 16:29:04 by viforget         ###   ########.fr       */
+/*   Updated: 2019/09/11 09:44:42 by viforget         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,32 @@ unsigned int	cnt_percent(const char *format)
 	return (p);
 }
 
-void			setfunction(char *st)
+void			setfunctionnum(const char *st, int size)
+{
+	
+}
+
+void			setfunction(const char *st, void *var)
+{
+	if (*st == 'h')
+	{
+		st++;
+		if (*st == 'h')
+			return (setfunctionnum(st + 1, 2));
+		return (setfunctionnum(st + 1, 1));
+
+	}
+	else if (*st == 'l')
+	{
+		st++;
+		if (*st == 'l')
+			return (setfunctionnum(st + 1, 32));
+		return (setfunctionnum(st + 1, 64));
+	}
+	if (*st == 's')
+		ft_putstr(*var);
+	setfunctionnum(st, 0);
+}
 
 int				ft_printf(const char *format, ...)
 {
@@ -50,7 +75,7 @@ int				ft_printf(const char *format, ...)
 		if (format[e] == '%')
 		{
 			write(1, format + s, e - s);
-			setfunction(format + e + 1]);
+			setfunction(format + e + 1);
 			e += 2;
 			s = e;
 		}
@@ -62,6 +87,6 @@ int				ft_printf(const char *format, ...)
 
 int 			main()
 {
-	ft_printf("HEY %dSALUT\n");	
+	ft_printf("HEY %dSAL%iUT\n");	
 	return (0);
 }
