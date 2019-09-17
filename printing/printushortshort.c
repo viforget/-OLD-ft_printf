9dca8ca9744cddfb71bed50e689824c6ae236479
Victor Forget <viforget@student.s19.be>
2019-09-17 10:27:15 +0200

    commit

---
+++ printing/printushortshort.c
@@ -0,0 +1,43 @@
+#include "includes/ft_printf.h"
+
+void	printushortshort2(char *st, unsigned int d, unsigned int neg, unsigned int base, short short n)
+{
+	char	*str;
+
+	if (!(str = (char *)malloc(sizeof(char) * d + neg + 1)))
+		return ;
+	str[d + neg] = 0;
+	str[0] = '-';
+	while (d + neg > neg)
+	{
+		str[d + neg - 1] = st[(n % base * -1)];
+		d--;
+		n /= base;
+	}
+	ft_putstr(str);
+	ft_strdel(&str);
+}
+
+void	printushortshort(short short n, char *st, unsigned int base)
+{
+	unsigned int		neg;
+	short short			nb;
+	unsigned int		d;
+
+	d = 0;
+	neg = 1;
+	if (n == 0)
+		return (ft_putchar('0'));
+	if (n >= 0)
+	{
+		neg = 0;
+		n *= -1;
+	}
+	nb = n;
+	while (nb != 0)
+	{
+		d++;
+		nb /= base;
+	}
+	printushortshort2(st, d, neg, base, n);
+}
